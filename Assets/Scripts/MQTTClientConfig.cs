dc0f4c7df35dbbb5382cbff44b5c2f85f583e2e4
詔鈞 梁 <bluewolf2735@hotmail.com>
2021-01-28 08:05:01 +0800

    Initial commit

---
+++ Assets/Scripts/MQTTClientConfig.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class MQTTClientConfig
+{
+    /// <summary>
+    /// Broker address
+    /// </summary>
+    public static string ServerUrl = "test.mosquitto.org";
+    
+    /// <summary>
+    /// Broker connect Port
+    /// </summary>
+    public static int Port = 1883;
+
+    /// <summary>
+    /// user name
+    /// </summary>
+    public static string UserName = "Zach";
+
+    /// <summary>
+    /// password
+    /// </summary>
+    public static string Password = "testmqtt";
+
+    /// <summary>
+    /// subscribing topic
+    /// </summary>
+    public static string SubscribeTopic = "MQTTNet_Unity/test";
+
+    /// <summary>
+    /// publishing topic
+    /// </summary>
+    public static string PublishTopic = "MQTTNet_Unity/test";
+
+    /// <summary>
+    /// need retained
+    /// </summary>
+    public static bool Retained = false;
+
+    /// <summary>
+    /// Quality of service
+    /// </summary>
+    public static int QoS = 0;
+}
